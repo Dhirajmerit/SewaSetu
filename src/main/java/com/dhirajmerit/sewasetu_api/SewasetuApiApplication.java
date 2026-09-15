ee83bcb0e3b94c092c8f03c960328a280fec3ecd
Dhiraj Yogesh Patil <dhiraj.patil1207@gmail.com>
2026-09-15 22:00:57 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/dhirajmerit/sewasetu_api/SewasetuApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhirajmerit.sewasetu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewasetuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewasetuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhirajmerit/sewasetu_api/SewasetuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajmerit.sewasetu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewasetuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
